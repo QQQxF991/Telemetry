1ac1b89eba3762abfa1407d8b0b07308ee850cb5
QQQxF991 <cyberfly.exe@gmail.com>
2025-12-11 13:36:47 +0300

    X64-86 Archlinux STABLE

--- main.cpp
+++ main.cpp
@@ -14,25 +14,44 @@ void Server() {
         close(SRV);
         return;   
     }
+    
     if (listen(SRV,5) < 0) {
         perror("listen");
         close(SRV); 
         return;
     }
+    
     std::cout << "Server listening port : " << BINARY_PORT << "\n";
     while (running) {
         sockaddr_in cli{};
         socklen_t client = sizeof(cli);
-        int cl = accept (srv,(sockaddr*)&cli,sizeof(clilen)) < 0)
+        int cl = accept (srv,(sockaddr*)&cli,&client);
+        if (cl < 0) {
+            if (running) {
+                perror("accept");
+                break;
+            }
+        }
+        std::thread([cl](){
+            std::vector<uint8_t> buf;
+            buf.reserve(1024);
+            while (true) {
+                uint8_t tmp[512];
+                ssize_t R = read(cl,tmp,sizeof(tmp));
+                if(R <= 0) break;
+                buf.insert(buf.end(),tmp,tmp+R);
+                while (buf.size() >= 15) {
+                    uint8_t msg[15];
+                    for (int i = 0; i < 15; ++i) msg[i] = buf[i];
+                    process_message(msg);
+                    buf.erase(buf.begin(),buf.begin() + 15);
+                } 
+            }
+             close(cl);
+        }).detach()   
     }
-    
-    
-    
-
-
+    close(SRV);
 }
-
-
 
 int main(int argc, char const *argv[]) {
     
