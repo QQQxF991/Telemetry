b9ceb579055a963e81846e38981ca63a35888291
QQQxF991 <cyberfly.exe@gmail.com>
2025-12-11 09:12:36 +0300

    X64-86 Archlinux STABLE

--- main.cpp
+++ main.cpp
@@ -5,9 +5,31 @@ void Server() {
     if (SRV < 0) {perror("Socket"); return;}
     int opt = 1; 
     setsocport = (srv,SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(BINARY_PORT);
+    if (bind(SRV,(sockaddr*)&addr,sizeof(addr)) < 0) {
+        perror("bind");
+        close(SRV);
+        return;   
+    }
+    if (listen(SRV,5) < 0) {
+        perror("listen");
+        close(SRV); 
+        return;
+    }
+    std::cout << "Server listening port : " << BINARY_PORT << "\n";
+    while (running) {
+        sockaddr_in cli{};
+        socklen_t client = sizeof(cli);
+        int cl = accept (srv,(sockaddr*)&cli,sizeof(clilen)) < 0)
+    }
     
+    
+    
+
+
 }
 
 
